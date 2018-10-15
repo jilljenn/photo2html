7f7667ebd243312c05c90ffa5bced589b41c5552
fnareoh <garance.gourdel@laposte.net>
2018-10-15 20:45:32 +0200

    Look at files in thumb to avoid redoing the thumns on an update.

--- example/run.py
+++ example/run.py
@@ -15,12 +15,21 @@ img {
 </style>
 '''
 
+all_photos = sorted(glob.glob('*.jpg'))
+not_in_thumb = []
+
 if not os.path.isdir('thumb'):
     os.makedirs('thumb')
+else:
+	thumb = os.popen("ls thumb/").read().splitlines()
+	not_in_thumb = [photo for photo in all_photos if not photo in thumb]
 
 html += '<div>\n'
-for photo in sorted(glob.glob('*.jpg')):
-    os.system('convert {:s} -auto-orient -resize 150 thumb/{:s}'.format(photo, photo))
+
+for photo in not_in_thumb:
+	os.system('convert {:s} -auto-orient -resize 150 thumb/{:s}'.format(photo, photo))
+
+for photo in all_photos:
     html += '<a href="{:s}"><img src="thumb/{:s}" /></a>\n'.format(photo, photo)
 html += '</div>'
 
